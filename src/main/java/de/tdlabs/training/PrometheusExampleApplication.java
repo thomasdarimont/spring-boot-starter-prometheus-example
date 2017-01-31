605a3d16b5c298619b256942443a265d2ccdda3a
Thomas Darimont <thomas.darimont@gmail.com>
2017-01-31 21:51:16 +0100

    Fix context bootstrap

--- src/main/java/de/tdlabs/training/PrometheusExampleApplication.java
+++ src/main/java/de/tdlabs/training/PrometheusExampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public interface PrometheusExampleApplication {
+public class PrometheusExampleApplication {
 
-    static void main(String... args) {
+    public static void main(String... args) {
         SpringApplication.run(PrometheusExampleApplication.class, args);
     }
 }
--- src/test/java/de/tdlabs/training/PrometheusExampleApplicationTests.java
+++ src/test/java/de/tdlabs/training/PrometheusExampleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootStarterPrometheusExampleApplicationTests {
+public class PrometheusExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
